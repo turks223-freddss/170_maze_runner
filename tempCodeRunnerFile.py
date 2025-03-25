701a7108fc37bdeb2ae1110d58d6a652de352369
jer2x <rulete.jeric@gmail.com>
2025-03-26 00:06:26 +0800

    Changed running logic, made master and player have separate buttons pero naa sa running na tanang logic nila pa check nalang ko balik

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+    #     skill_2_color = ACTIVE_SKILL_COLOR
+    # elif maze_skill_2_used:
+    #     skill_2_color = GRAY
+    # else:
+    #     skill_2_color = BLUE
+    # pygame.draw.rect(screen, skill_2_color, skill_2_button)
